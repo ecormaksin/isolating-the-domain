6f5d6e570b5aeacafe2a70afde5a7a4c23416654
ecormaksin <turuth_jp_4133@yahoo.co.jp>
2020-03-21 13:29:42 +0900

    Revert "環境変数をコンソールに出力してみる"
    This reverts commit f5121022974b7b09945a6818f71d74216d1d6784.

--- webapp/src/main/java/example/Application.java
+++ webapp/src/main/java/example/Application.java
@@ -5,20 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-	public static void main(String[] args) {
-		outputEnvVar("DATABASE_URL");
-		outputEnvVar("JDBC_DATABASE_URL");
-		outputEnvVar("JDBC_DATABASE_USERNAME");
-		outputEnvVar("JDBC_DATABASE_PASSWORD");
-		outputEnvVar("SPRING_DATASOURCE_URL");
-		outputEnvVar("SPRING_DATASOURCE_USERNAME");
-		outputEnvVar("SPRING_DATASOURCE_PASSWORD");
-
-		SpringApplication.run(Application.class, args);
-	}
-	
-	private static void outputEnvVar(String key) {
-		if (null == System.getenv(key)) return;
-		System.out.println(key + ": " + System.getenv(key));
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
